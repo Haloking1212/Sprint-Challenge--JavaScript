2278fc5fe6dcadc1c8b1de3d70789eb408262dee
Hector Valdez <hvaldez180@gmail.com>
2019-07-26 19:43:26 -0400

    updated bugs

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,12 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
-function dividingNums(x,y,cb) {
-  cb(x / y);
+function consume(x,y,cb) {
+  cb(x, y);
 }
-dividingNums(50,5,(dividing) => {
-  console.log('This is dividing x and y:',dividing);
-});
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -21,31 +18,26 @@ dividingNums(50,5,(dividing) => {
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
-function add (x,y,consume){
-  consume(x + y);
+function add (x,y){
+  console.log(x + y);
+  return(x + y);
 }
-add(2,2, (adding) => {
-  console.log('This is adding x and y with consume as the cb:',adding);
-});
 
-function multiply (x,y,consume){
-  consume(x * y);
+function multiply (x,y){
+  console.log(x * y);
+  return (x * y);
+  
 }
- multiply(10,16, (multiplying) => {
-   console.log('This is mulitplying x and y with consume as the cb:',multiplying);
-});
 
-function greeting(first,last,consume){
-  consume(first + ' ' + last)
+function greeting(first,last){
+  console.log(`Hello ${first} ${last}, nice to meet you!`)
+  return (first, last)
 }
-greeting('Mary', 'Poppins', (greet) => {
-  console.log(`Hello ${greeting.first} ${greeting.last}, nice to meet you!`,greet)
-});
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
